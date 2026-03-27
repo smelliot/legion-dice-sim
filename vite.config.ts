7628bc5bf657392d59e539b8d8a92eec1d9c252d
Elliot Thomas <elliot@hyperexponential.com>
2026-03-27 11:00:54 +0000

    Fix pages sub-path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/legion-dice-sim/",
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: 'build',
+    outDir: "build",
   },
 });
